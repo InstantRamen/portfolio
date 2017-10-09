1279de5705cd7246cff2cf4dc2e0dbbf11728281
Derrick Cudd <instantramen125@gmail.com>
2017-10-09 09:41:07 +0000

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,8 @@ import {Menu} from 'semantic-ui-react';
 
 const Header = props => {
   return (
-    <Menu attached size='massive'>
-      <Menu.Item header>
+    <Menu attached size='massive' borderless inverted>
+      <Menu.Item header as='h2' link>
         Derrick Cudd
       </Menu.Item>
     </Menu>  
