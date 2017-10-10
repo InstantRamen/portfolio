e542afa0bfa5d06372816c0e88162d1ce03ce50b
Derrick Cudd <instantramen125@gmail.com>
2017-10-10 08:45:41 +0000

    add nav links to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import {Menu} from 'semantic-ui-react';
 
+import { Link } from 'react-router-dom';
+
 const Header = props => {
   return (
-    <Menu attached size='massive' borderless inverted>
-      <Menu.Item header as='h2' link>
+    <Menu attached size='large' borderless inverted>
+      <Menu.Item header as={Link} to='/'>
         Derrick Cudd
       </Menu.Item>
+      <Menu.Menu position='right' >
+        <Menu.Item as={Link} to='/projects'>Projects</Menu.Item>
+        <Menu.Item as={Link} to='/'>About Me</Menu.Item>
+      </Menu.Menu>
     </Menu>  
   );
 }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import { Route } from 'react-router-dom';
 
 import Header from '../../components/Header/Header';
 import About  from '../../containers/About/About';
