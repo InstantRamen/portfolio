671f69cd88baefcd561e09f5094d16db67b9171e
Derrick Cudd <instantramen125@gmail.com>
2017-10-11 02:52:48 +0000

    reformat data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,109 +1,114 @@
 export default {
-  "info": {
-    "name": "Derrick Cudd"
+  info: {
+    name: "Derrick Cudd"
   },
-  "about": {
-    "title": "I'm not Derrick",
-    "body": "I'm a developer with a primary focus on web technologies, namely Node/Express on the backend, and React on the frontend. As it stands, I have at least basic experience in React, Node, Ruby/Rails, Java, Python, and C#."
+  about: {
+    title: "I'm Derrick",
+    body: `
+    I'm a developer with a primary focus on web technologies,
+    namely Node/Express on the backend, and React on the frontend.
+    As it stands, I have at least basic experience in React, Node, Ruby/Rails,
+    Java, Python, and C#.
+    `
   },
-  "all_tags": [{
-      "key": "react",
-      "name": "React"
+  all_tags: [{
+      key: "react",
+      name: "React"
     },
     {
-      "key": "javascript",
-      "name": "JavaScript"
+      key: "javascript",
+      name: "JavaScript"
     },
     {
-      "key": "node",
-      "name": "NodeJS"
+      key: "node",
+      name: "NodeJS"
     },
     {
-      "key": "fullstack",
-      "name": "Fullstack"
+      key: "fullstack",
+      name: "Fullstack"
     },
     {
-      "key": "front-end",
-      "name": "Front End"
+      key: "front-end",
+      name: "Front End"
     },
     {
-      "key": "back-end",
-      "name": "Back End"
+      key: "back-end",
+      name: "Back End"
     },
     {
-      "key": "mongodb",
-      "name": "MongoDB"
+      key: "mongodb",
+      name: "MongoDB"
     },
     {
-      "key": "mvc",
-      "name": "MVC"
+      key: "mvc",
+      name: "MVC"
     },
     {
-      "key": "express",
-      "name": "Express"
+      key: "express",
+      name: "Express"
     },
     {
-      "key": "ruby",
-      "name": "Ruby"
+      key: "ruby",
+      name: "Ruby"
     },
     {
-      "key": "rails",
-      "name": "Ruby on Rails"
+      key: "rails",
+      name: "Ruby on Rails"
     },
     {
-      "key": "game",
-      "name": "Game"
+      key: "game",
+      name: "Game"
     }
   ],
-  "projects": [
+  projects: [
     {
-      "name": "Calculator",
-      "about": "A simple calculator",
-      "tags": ["react", "javascript", "front-end"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "Calculator",
+      about: "A simple calculator",
+      tags: ["react", "javascript", "front-end"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "Todo App",
-      "about": "A simple calculator",
-      "tags": ["react", "javascript", "fullstack", "node", "mongodb", "express"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "Todo App",
+      about: "A simple calculator",
+      tags: ["react", "javascript", "fullstack", "node", "mongodb", "express"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "Portfolio",
-      "about": "This portfolio",
-      "tags": ["react", "javascript", "front-end"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "Portfolio",
+      about: "This portfolio",
+      tags: ["react", "javascript", "front-end"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "eMayl",
-      "about": "basic email campaign app",
-      "tags": ["react", "javascript", "fullstack", "node", "mongodb", "express"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "eMayl",
+      about: "basic email campaign app",
+      tags: ["react", "javascript", "fullstack", "node", "mongodb", "express"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "TicTacToe",
-      "about": "Command line TicTacToe game with an AI that never loses",
-      "tags": ["ruby", "cli", "game"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "TicTacToe",
+      about: "Command line TicTacToe game with an AI that never loses",
+      tags: ["ruby", "cli", "game"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "Cosmos",
-      "about": "A basic flat-file CMS",
-      "tags": ["react", "javascript", "fullstack", "node", "express"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "Cosmos",
+      about: "A basic flat-file CMS",
+      tags: ["react", "javascript", "fullstack", "node", "express"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     },
     {
-      "name": "Game of Life",
-      "about": "A javascript implementation of Conway's Game of Life",
-      "tags": ["react", "javascript", "front-end", "game"],
-      "source": "https://github.com/InstantRamen",
-      "live": "/"
+      name: "Game of Life",
+      about: "A javascript implementation of Conway's Game of Life",
+      tags: ["react", "javascript", "front-end", "game"],
+      source: "https://github.com/InstantRamen",
+      live: "/"
     }
   ]
 }
